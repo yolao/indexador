358c3d973d4b63f32afce2ff446ea8066e3e275a
aivdna <aivdna@72bf7af7-be60-ff82-d14a-a83f3b47635d>
2011-06-11 17:07:34 +0000

    Primer avance de la tarea, ese codigo obtiene el texto que haya dentro del documento que se le envia... XD

---
+++ indexadorRI2011/src/indexadorri2011/IndexadorRI2011.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indexadorri2011;
+
+/**
+ *
+ * @author Aaron
+ */
+public class IndexadorRI2011 {
+
+    /**
+     * @param args the command line arguments
+     */
+   /* public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+}
